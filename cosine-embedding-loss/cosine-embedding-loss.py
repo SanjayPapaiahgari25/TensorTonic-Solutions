490bb734d0b952c6958653d56af9c6defe3497d7
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2026-04-10 08:45:47 +0530

    Update solution for cosine-embedding-loss

--- cosine-embedding-loss/cosine-embedding-loss.py
+++ cosine-embedding-loss/cosine-embedding-loss.py
@@ -6,6 +6,12 @@ def cosine_embedding_loss(x1, x2, label, margin):
     # Write code here
     x1_norm = np.linalg.norm(x1)
     x2_norm = np.linalg.norm(x2)
-    eps = 1e-8
-    cosine = np.dot(x1, x2)/(x1_norm * x2_norm + eps)
-    return np.mean(np.where(label==1, 1-cosine, np.maximum(0, cosine - margin)))
+    # epsilon to be added to the denominator
+    cosine = np.dot(x1, x2) / (x1_norm * x2_norm)
+    return np.mean(
+        np.where(
+            label == 1,
+            1 - cosine,
+            np.maximum(0, cosine - margin)
+        )
+    )
