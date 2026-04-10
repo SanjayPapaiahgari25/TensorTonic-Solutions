aff433046816eb61bf8b30da96d8a206029cad80
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2026-04-10 08:40:03 +0530

    Add solution for cosine-embedding-loss

---
+++ cosine-embedding-loss/cosine-embedding-loss.py
@@ -0,0 +1,10 @@
+import numpy as np
+def cosine_embedding_loss(x1, x2, label, margin):
+    """
+    Compute cosine embedding loss for a pair of vectors.
+    """
+    # Write code here
+    x1_norm = np.linalg.norm(x1)
+    x2_norm = np.linalg.norm(x2)
+    cosine = np.dot(x1, x2)/(x1_norm*x2_norm)
+    return np.mean(np.where(label==1, 1-cosine, np.maximum(0, cosine - margin)))
