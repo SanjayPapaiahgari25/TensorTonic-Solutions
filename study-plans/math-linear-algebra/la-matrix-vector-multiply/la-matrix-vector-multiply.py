a868debb36a59cfb7996b0d98a2ef609085339bc
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2026-08-25 15:15:07 +0530

    Solve la-matrix-vector-multiply

---
+++ study-plans/math-linear-algebra/la-matrix-vector-multiply/la-matrix-vector-multiply.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def matrix_vector_multiply(A, x):
+    """
+    Returns: 1-D float64 array, the product A @ x.
+    """
+    A = np.asarray(A)
+    x = np.asarray(x)
+    return A @ x
