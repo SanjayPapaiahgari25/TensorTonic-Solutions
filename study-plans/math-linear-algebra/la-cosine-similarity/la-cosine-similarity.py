d2e2d06abf3858da8fb82ef4e3a95d5c04c31a6a
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2026-08-25 14:31:54 +0530

    Solve la-cosine-similarity

--- study-plans/math-linear-algebra/la-cosine-similarity/la-cosine-similarity.py
+++ study-plans/math-linear-algebra/la-cosine-similarity/la-cosine-similarity.py
@@ -6,10 +6,14 @@ def cosine_similarity(a, b):
     """
     a = np.asarray(a)
     b = np.asarray(b)
+    
     if a.shape != b.shape:
         raise ValueError("Input shapes must match")
+    
     a_norm = np.sqrt(np.sum(a**2))
     b_norm = np.sqrt(np.sum(b**2))
+    
     if a_norm == 0 or b_norm == 0:
         return 0.0
+    
     return np.dot(a, b)/(a_norm * b_norm)
