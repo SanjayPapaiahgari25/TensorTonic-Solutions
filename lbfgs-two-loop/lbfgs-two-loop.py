acd53942b65cdf2b4f43e40569823a81d215b63c
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2026-08-31 23:52:37 +0530

    Solve lbfgs-two-loop

--- lbfgs-two-loop/lbfgs-two-loop.py
+++ lbfgs-two-loop/lbfgs-two-loop.py
@@ -12,28 +12,20 @@ def lbfgs_direction(grad: list, s_list: list, y_list: list) -> list:
 
     rho = np.zeros(m)
     alpha = np.zeros(m)
-
-    # First loop
     for i in range(m - 1, -1, -1):
-        rho[i] = 1.0 / np.dot(y_list[i], s_list[i])
-        alpha[i] = rho[i] * np.dot(s_list[i], q)
+        rho_i = (1.0 / np.dot(y_list[i], s_list[i])).item()
 
-        q = q - alpha[i] * y_list[i]
+        alpha_i = (rho_i * np.dot(s_list[i], q)).item()
 
-    # Initial Hessian approximation H_0
-    gamma = (
-        np.dot(s_list[m - 1], y_list[m - 1])
-        / np.dot(y_list[m - 1], y_list[m - 1])
-    )
+        rho[i]=rho_i
+        alpha[i]=alpha_i
 
-    r = gamma * q
+        q = q - alpha_i * y_list[i]
 
-    # Second loop
+    r = (np.dot(s_list[m-1], y_list[m-1])/np.dot(y_list[m-1], y_list[m-1]))*q
+    
     for i in range(m):
-        beta_i = rho[i] * np.dot(y_list[i], r)
-        r = r + s_list[i] * (alpha[i] - beta_i)
-
-    # Descent direction
+        beta_i = rho[i]*np.dot(y_list[i], r)
+        r = r + s_list[i]*(alpha[i] - beta_i)
     r = -r
-
     return r.tolist()
