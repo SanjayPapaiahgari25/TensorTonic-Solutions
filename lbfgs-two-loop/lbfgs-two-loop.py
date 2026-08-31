8e62a53f847a94e28bb09ff65d2b39df5dd36b8d
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2026-08-31 23:54:47 +0530

    Solve lbfgs-two-loop

--- lbfgs-two-loop/lbfgs-two-loop.py
+++ lbfgs-two-loop/lbfgs-two-loop.py
@@ -7,25 +7,22 @@ def lbfgs_direction(grad: list, s_list: list, y_list: list) -> list:
     m = len(s_list)
 
     q = np.asarray(grad, dtype=float)
-    s_list = [np.asarray(s, dtype=float) for s in s_list]
-    y_list = [np.asarray(y, dtype=float) for y in y_list]
+    s_list = np.asarray(s_list, dtype=float)
+    y_list = np.asarray(y_list, dtype=float)
 
     rho = np.zeros(m)
     alpha = np.zeros(m)
+    
     for i in range(m - 1, -1, -1):
         rho_i = (1.0 / np.dot(y_list[i], s_list[i])).item()
-
         alpha_i = (rho_i * np.dot(s_list[i], q)).item()
-
         rho[i]=rho_i
         alpha[i]=alpha_i
-
         q = q - alpha_i * y_list[i]
 
     r = (np.dot(s_list[m-1], y_list[m-1])/np.dot(y_list[m-1], y_list[m-1]))*q
     
     for i in range(m):
-        beta_i = rho[i]*np.dot(y_list[i], r)
-        r = r + s_list[i]*(alpha[i] - beta_i)
+        r = r + s_list[i]*(alpha[i] - (rho[i]*np.dot(y_list[i], r)))
     r = -r
     return r.tolist()
