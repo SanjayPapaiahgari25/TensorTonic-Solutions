9c559246ae76882b1bda215e22e425365a7803ce
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2026-09-06 16:13:01 +0530

    Solve info-nce-loss

--- info-nce-loss/info-nce-loss.py
+++ info-nce-loss/info-nce-loss.py
@@ -9,10 +9,11 @@ def info_nce_loss(Z1: list, Z2: list, temperature: float = 0.1) -> float:
     Z2 = np.asarray(Z2)
     n = Z1.shape[0]
     S = (Z1@Z2.T)/temperature
-
-    p=[0 for i in range(n)]
-
+    p = []
+    
     for i in range(n):
-        p[i] = np.exp(S[i, i]-np.max(S[i]))/np.sum(np.exp(S[i]-np.max(S[i])), axis=-1)
+        p.append(np.exp(S[i, i]-np.max(S[i]))/np.sum(np.exp(S[i]-np.max(S[i])), axis=-1))
+    
     L = -np.mean(np.log(p))
+    
     return float(L)
